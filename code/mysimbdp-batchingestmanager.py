395db958f9813c85ebdebcc933ac5138569dbbe9
Hung Nguyen <hungnguyen10897@gmail.com>
2022-03-15 06:25:42 +0200

    Restructure tenants registration, implement logging

--- code/mysimbdp-batchingestmanager.py
+++ code/mysimbdp-batchingestmanager.py
@@ -1,6 +1,9 @@
-import os
+from operator import le
+import os, configparser, json, logging
+from tenant import Tenant
 
-# Tenants discovery
+
+# Tenant's directories discovery
 def scan_tenants():
   dirs = next(os.walk("./tenants", topdown=True))[1]
   tenants = list(filter(lambda x: x!="shared", dirs))
@@ -8,7 +11,21 @@ def scan_tenants():
 
 
 if __name__ == "__main__":
-  tenants = scan_tenants()
-  for tenant in tenants:
-    exec(f"from tenants.{tenant}.clientbatchingestapp import print_tenant")
-    print_tenant()
+
+  logging.basicConfig(filename='platform.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level="INFO")
+
+  config = configparser.ConfigParser()
+  config.read("./mysimbdp-batchingestmanager.cfg")
+  
+  available_tenants = scan_tenants()
+  tenants_in_use = json.loads(config.get("DEFAULT", "tenants"))
+
+  logging.info(f"Tenants Present in the platform: {available_tenants}")
+  logging.info(f"Tenants Registered in the platform: {tenants_in_use}")
+  
+  for tenant in tenants_in_use:
+    if tenant not in available_tenants:
+      continue
+    tenant_obj = Tenant(tenant)
+    tenant_obj.batch_ingest()
+
--- code/tenants/gift_card/clientbatchingestapp.py
+++ code/tenants/gift_card/clientbatchingestapp.py
@@ -1,41 +1,8 @@
-# Run from code/: python tenants/gift_card/clientbatchingestapp.py
-
-from tenants.shared.utils import parse_ingest_config, get_batches, parse_cassandra_config
-from tenants.shared.io import get_cassandra_session, read_batch_df, write_df_to_cassandra
-from pathlib import Path
 import pandas as pd
-
-def print_tenant():
-  tenant_dir = Path(__file__).parent
-  print(tenant_dir)
-
 
 def process_batch(batch_df):
   # Filter non-purchase, non-helpful reviews
   processed_df = batch_df[(batch_df["helpful_votes"] != 0) & (batch_df["verified_purchase"] == 'Y')]
+  processed_df.fillna("", inplace=True)
 
   return processed_df
-
-if __name__ == "__main__":
-
-  tenant_dir = Path(__file__).parent
-  tenant_config_path = tenant_dir.joinpath("clientbatchingestapp.cfg")
-  tenant_staging_path = tenant_dir.joinpath("staging")
-
-  ingest_config = parse_ingest_config(tenant_config_path)
-  cassandra_config = parse_cassandra_config(tenant_config_path)
-
-  session = get_cassandra_session(cassandra_config)
-  session.execute("USE mysimbdp")
-
-  batches = get_batches(tenant_staging_path, ingest_config)
-
-  for batch in batches:
-    print(batch)
-    batch_df = read_batch_df(batch)
-
-    processed_batch_df = process_batch(batch_df)
-
-    write_df_to_cassandra(session, processed_batch_df)
-
-  session.close()
